37b8d5bf64b9a37d7a1494aa8f782cb86a4ccc94
Ricardo Salgado <ricardodsalg@gmail.com>
2025-01-08 17:25:07 -0500

    connect js
    connecting JS

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 // connect
-const inputName = document.querySelector('#name');
+const inputName = document.querySelector('input #name');
 const punchInBtn = document.querySelector('#punchin');
 const punchOutBtn = document.querySelector('#punchout');
+// const sheetUpdate = fetch('https://api.sheetmonkey.io/form/4zJFSXmt6xq7gLgUFnPFrm') {
+//     method: 'POST',
 
+// }
 
 // get the input
 
 
 // button
 punchInBtn.addEventListener('click', (e) =>{
-    console.log('Punched in')
+    e.preventDefault();
+    console.log(`Punched in ${inputName}`)
 })
 
-punchOutBtn.addEventListener('click', (e) =>{
-    console.log('Punched out')
-})
+// punchOutBtn.addEventListener('click', (e) =>{
+//     console.log(`Punched out ${inputName}`)
+// })
 
 // functions
