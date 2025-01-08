5b716b225c23e5537855f65a24bf02ccb0f4fe47
Ricardo Salgado <ricardodsalg@gmail.com>
2025-01-08 15:23:30 -0500

    button animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('conectou')
+// connect
+const inputName = document.querySelector('#name');
+const punchInBtn = document.querySelector('#punchin');
+const punchOutBtn = document.querySelector('#punchout');
+
+
+// get the input
+
+
+// button
+punchInBtn.addEventListener('click', (e) =>{
+    console.log('Punched in')
+})
+
+punchOutBtn.addEventListener('click', (e) =>{
+    console.log('Punched out')
+})
+
+// functions
