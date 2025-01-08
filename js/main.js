b14fe0a58dc0b24777c4c41a4e0cbcfb5053f1b2
Ricardo Salgado <ricardodsalg@gmail.com>
2025-01-08 18:18:31 -0500

    Date and time
    setting date and time

--- js/main.js
+++ js/main.js
@@ -1,23 +1,67 @@
 // connect
-const inputName = document.querySelector('input #name');
+const inputName = document.querySelector('#name');
 const punchInBtn = document.querySelector('#punchin');
 const punchOutBtn = document.querySelector('#punchout');
+const now = new Date();
+const date = now.toLocaleDateString();
+const time = now.toLocaleTimeString();
 // const sheetUpdate = fetch('https://api.sheetmonkey.io/form/4zJFSXmt6xq7gLgUFnPFrm') {
 //     method: 'POST',
 
 // }
 
+// functions
+function getLocation(callback) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            callback(latitude, longitude);
+        }, (error) => {
+            console.error(`Error to take the location ${error.message}`);
+            callback(null);
+        });
+    } else {
+        console.error('Geolocation is not supported by your browser');
+        callback(null);
+
+    }
+}
+
+
+function getCurrentDateTime() {
+    return { date, time };
+}
 // get the input
 
 
 // button
-punchInBtn.addEventListener('click', (e) =>{
+punchInBtn.addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(`Punched in ${inputName}`)
+    getCurrentDateTime()
+    const employeeName = inputName.value;
+
+    console.log(employeeName, date, time);
+
+    if (employeeName.trim() === "") {
+        console.log("Field name is empty, please entry a valid name.");
+        alert('Field name is empty, please entry a valid name.')
+    } else {
+        console.log(`Punched in: ${employeeName}`);
+    }
+
+
 })
 
-// punchOutBtn.addEventListener('click', (e) =>{
-//     console.log(`Punched out ${inputName}`)
-// })
+punchOutBtn.addEventListener('click', (e) => {
+    e.preventDefault();
 
-// functions
+    const employeeName = inputName.value;
+
+    if (employeeName.trim() === "") {
+        console.log("Field name is empty, please entry a valid name.");
+        alert('Field name is empty, please entry a valid name.')
+    } else {
+        console.log(`Punched in: ${employeeName}`);
+    }
+})
